96dee0d3f8d8a51eee4f2698344040315482f6d9
Pornticha <68775548+Porntichanice@users.noreply.github.com>
2022-02-02 13:07:04 +0700

    lab

--- basic-reactive/src/App-computed.vue
+++ basic-reactive/src/App-computed.vue
@@ -4,17 +4,19 @@ console.clear()
 const base = ref(4)
 const height = ref(8)
 const result = computed(() => {
-  return (1 / 2) * base.value * height.value
+    return (1 / 2) * base.value * height.value
 })
 
 </script>
 
 <template>
   <p>Base: {{ base }}</p>
-  <input type="number" v-model="base" />
+  <button @click="base++">Add 1 to Base</button>
   <p>Height: {{ height }}</p>
-  <input type="number" v-model="height" />
+  <button @click="height++">Add 1 to Height</button>
+
   <p>Area Result: {{ result }}</p>
 </template>
+
 
 <style></style>
---
+++ basic-reactive/src/App-copy.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue'
+console.clear()
+const msg = 'Hello'
+// counter is a reactive variable
+const counter = ref(1)
+// setTimeout(() => console.log(++counter), 3000)
+setInterval(() => console.log(++counter.value), 1000)
+</script>
+ 
+<template>
+    <p>Counter: {{ counter}}</p>
+</template>
+ 
+<style>
+
+</style>
