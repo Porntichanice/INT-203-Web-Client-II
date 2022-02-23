a6a2057d6db4ed5d03b35b71dd5f5b17c1ca56a5
Pornticha <68775548+Porntichanice@users.noreply.github.com>
2022-02-23 11:01:08 +0700

    new

---
+++ basic-dynamic/src/App-bin.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { resolveDirective } from 'vue'
+
+const warningColor = 'orange'
+const successText = 'color:green'
+const warningText = 'color:orange'
+const italicText = 'italic'
+const decorText = 'text-decoration:underline'
+const isError = false
+const isDecor = false
+
+const dangerText = {color:red, 'font-style': italic}
+const dangerText2 = {color: 'red', 'font-style': 'italic' }
+</script>
+ 
+<template>
+<!-- style attribute with Object Syntax-->
+<p :style="{color: warningColor, 'font-style': italicText }">
+  this is style with object syntax
+</p>
+<p :style="isError ? 'color:red' : 'color:green'">this is error message</p>
+<p :style="isError ? successText : warningText">this is binding on varible</p>
+
+<!-- style attribute with array Syntax-->
+<p :style="[successText, decorText]">this is style with array syntax</p>
+<p :style="[successText, isDecor ? decorText :'']">
+this is style on array syntax with condition
+</p>
+<p :style="dangerText">danger text</p>
+<p :style="dangerText2">danger text2</p>
+</template>
+
+<style>
+</style>
---
+++ basic-template/src/App-copy.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+ 
+<template>
+<h1 class="text-green-700">Hello, Tailwind CSS</h1>
+<p class="text-uppercase">Hello, bootStrap</p>
+</template>
+ 
+<style>
+
+</style>
