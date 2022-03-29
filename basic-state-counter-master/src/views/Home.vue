62603c6ca45bfb7f3cdbf8d40958d942db1be52e
Pornticha <68775548+Porntichanice@users.noreply.github.com>
2022-03-29 22:30:08 +0700

    new

---
+++ basic-state-counter-master/src/views/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useCounter } from '../stores/counter.js';
+
+const myCounter=useCounter()
+const { doubleCounter } = storeToRefs(myCounter)
+console.log(doubleCounter.value)
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Counter (Home.vue): {{ myCounter.count }}</p>
+    <button @click="myCounter.addCounter">+1</button>
+  </div>
+</template>
+
+<style></style>
